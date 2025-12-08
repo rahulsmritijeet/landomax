1746ea89c3ad52a9f239f8220b79a9a7a6f9216a
Rahul Smritijeet <rahulsmritijeet@gmail.com>
2025-12-08 09:59:43 +0530

    Create

---
+++ js/config.js
@@ -0,0 +1,7 @@
+
+
+// Replace this with your deployed Google Apps Script Web App URL
+const API_URL = 'https://script.google.com/macros/s/AKfycbxi05ZyXRM4V-vrHI7qyPQbAe9BzzBKEWIRd1Vwfj_bUSInBnwMgknhzR_F36D8UCM3/exec';
+
+// Status options for orders
+const ORDER_STATUS_OPTIONS = ['Ordered', 'Shipped', 'Delivered', 'Cancelled'];
