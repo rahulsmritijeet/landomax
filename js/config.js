d8c814ac01e5a652fc1f5187f2e0da6a917fe87a
Rahul Smritijeet <rahulsmritijeet@gmail.com>
2025-12-08 10:51:50 +0530

    Update API_URL to new Google Apps Script URL

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 
 
 // Replace this with your deployed Google Apps Script Web App URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbxi05ZyXRM4V-vrHI7qyPQbAe9BzzBKEWIRd1Vwfj_bUSInBnwMgknhzR_F36D8UCM3/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzHxXCYWTSP5IzF8uJLTjwnwRF-C_-DSd8b7K5idQsS6_18pnXeA8xtSzsV-MsCLcEP/exec';
 
 // Status options for orders
 const ORDER_STATUS_OPTIONS = ['Ordered', 'Shipped', 'Delivered', 'Cancelled'];
