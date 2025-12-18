7a987d832474a0b767f640e637aee47f433fbb0d
Rahul Smritijeet <rahulsmritijeet@gmail.com>
2025-12-18 10:15:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // =====================================================
 
 // Your Google Apps Script Web App URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbxMCRkgMPxhn2ErBdXszakYgV3XmXyq74HA5yb4__P1bW3J0v3CQceo6i1Qjsw0phd_3w/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxFp5LrNy2dpLI6BR7AL1FN2W-PQVtQOgJk9bDxdlO_LfLxNxEr6BPr9YSM2bXEk1DUFg/exec';
 
 // Order status options
 const ORDER_STATUS_OPTIONS = ['Ordered', 'Shipped', 'Delivered', 'Completed', 'Cancelled'];
